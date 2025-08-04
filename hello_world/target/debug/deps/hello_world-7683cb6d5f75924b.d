37ca296c41e19de4ad9046fe6972f7c292c505c1
ethicalmentor <ethicalmentor101@gmail.com>
2025-08-05 00:26:08 +0530

    New_Update_Sample

---
+++ hello_world/target/debug/deps/hello_world-7683cb6d5f75924b.d
@@ -0,0 +1,5 @@
+/home/ethicalmentor/Programming/Git-EM/New_Projects_2026/hello_world/target/debug/deps/hello_world-7683cb6d5f75924b.d: src/main.rs
+
+/home/ethicalmentor/Programming/Git-EM/New_Projects_2026/hello_world/target/debug/deps/hello_world-7683cb6d5f75924b: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/ethicalmentor/Programming/Git-EM/New_Projects_2026/hello_world/target/debug/hello_world: /home/ethicalmentor/Programming/Git-EM/New_Projects_2026/hello_world/src/main.rs
